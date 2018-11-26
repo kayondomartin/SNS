1b5622a3a1f34799817ac17b602fe4d892ca563f
root <kayondomartin45@gmail.com>
2018-11-27 00:38:09 +0900

    not much

--- app/src/main/java/com/example/martinkayondo/sns/LoginActivity.java
+++ app/src/main/java/com/example/martinkayondo/sns/LoginActivity.java
@@ -1,13 +1,28 @@
 package com.example.martinkayondo.sns;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView needNewAccountLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        needNewAccountLink = findViewById(R.id.no_account_text);
+
+        needNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
